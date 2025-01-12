81faade474b8752fd0f57ad0f87ec634431dbec4
annabertani <64985847+annabertani@users.noreply.github.com>
2025-01-12 19:47:13 +0000

    Deploying to gh-pages from @ annabertani/annabertani.github.io@ed140d3691de4b2ebde115f93425b131d51eeffb 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -96,20 +96,6 @@ ninja.data = [{
           window.open("https://webapps.unitn.it/du/it/Persona/PER0193445/Pubblicazioni", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("", "_blank");
-        },
-      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
